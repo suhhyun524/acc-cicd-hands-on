ea7dc04916d7a2cff4b4f743f87f91fadbdf3c9d
julia-heo <100216331+julia-heo@users.noreply.github.com>
2024-05-02 00:44:59 +0900

    test

--- djangoProject/settings/prod.py
+++ djangoProject/settings/prod.py
@@ -3,18 +3,6 @@ import os
 DEBUG = False
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS')
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': env('DATABASE_NAME'),
-#         'USER': env('DATABASE_USER'),
-#         'PASSWORD': env('DATABASE_PASSWORD'),
-#         'HOST': env('DATABASE_HOST'),
-#         'PORT': env('DATABASE_PORT'),
-#         'OPTIONS': {"charset": "utf8mb4"},
-#     }
-# }
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DEBUG = True
 DATABASES = {
